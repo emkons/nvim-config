092a1dd8ca3f87c037c9f9339b318b6539074d6d
Emils Melders <emils.melders@datigroup.com>
2023-12-03 14:32:59 +0200

    Initial commit

---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "akinsho/toggleterm.nvim",
+    keys = {
+      {
+        "<leader>tt",
+        "<cmd>ToggleTerm size=20 direction=horizontal<cr>",
+        desc = "Open a horizontal terminal at the Desktop directory",
+      },
+    },
+    config = true,
+  },
+}
